4c71920ad445088566ba01ee154ac752d5bd40c2
KacpiCzeci <kapixxx666@wp.pl>
2019-05-12 00:38:59 +0200

    ok

--- particle.py
+++ particle.py
@@ -8,5 +8,22 @@ class Particle:
         self.Vx = Vx
         self.Vy = Vy
 
+def wall(self):
+    if self.x > width - self.size:
+        self.x = 2 * (width - size) - self.x
+        self.Vx = -self.Vx
+    
+    elif self.x < self.size:
+        self.x = 2 * size - self.x
+        self.Vx = -self.Vx
+    
+    if self.y > height - self.size:
+        self.y = 2 * (height - size) - self.y
+        self.Vy = -self.Vy
+    
+    elif self.y < self.size:
+        self.y = 2 * size - self.y
+        self.Vy = -self.Vy
+
     def Update(self):
         pass
