e9f1ac10033ecb717c8b68223791645af526dd39
Aleksander Szamałek <aleksander.szamalek@gmail.com>
2019-05-12 00:41:53 +0200

    update to gitignore

--- tile.py
+++ tile.py
@@ -4,8 +4,11 @@ class Tile:
     def __init__(self,x,y):
         self.x = x 
         self.y = y
-        self.Particles = []
+        self.particles = []
 
 
     def __hash__(self):
         return hash(self.x + self.y)
+
+    def __eq__(self,other):
+        return self.x == other.x and self.y == other.y
--- world.py
+++ world.py
@@ -9,22 +9,39 @@ class World:
         self.N = N
         self.R = R
 
-        self.Particles = []
-        self.Tiles = set()
+        self.particles = []
+        self.tiles = {}
 
         for i in range(constants.WORLD_SIZE):
-            self.Particles.append(Particle(0,i,random.randint(-constants.MAX_SPEED,constants.MAX_SPEED),random.randint(-constants.MAX_SPEED,constants.MAX_SPEED)))
+            self.particles.append(Particle(0,i,random.randint(-constants.MAX_SPEED,constants.MAX_SPEED),random.randint(-constants.MAX_SPEED,constants.MAX_SPEED)))
 
 
     def update(self):
         pass
 
     def create_world_array(self):
-        pass
+        
+        for p in self.particles:
+            self.assign_tile(p)
+
+    def create_collision_array(self,particle):
+        
+        collision_array = []
+        tile_coordinates = self.get_tile_coordinates(particle)
+        tile_hash = hash(tile_coordinates[0] + tile_coordinates[1])
+
+        
+
 
     def assign_tile(self,particle):
         coordinates = self.get_tile_coordinates(particle)
         particle_tile = Tile(coordinates[0],coordinates[1])
+
+        if particle_tile.__hash__() in self.tiles:
+            self.tiles[particle_tile.__hash__()].particles.append(particle)
+        else:
+            particle_tile.particles.append(particle)
+            self.tiles[particle_tile.__hash__()] = particle_tile
 
 
     def get_tile_coordinates(self,particle):
@@ -32,6 +49,3 @@ class World:
         y = math.floor(particle.y)
 
         return [x,y]
-
-    def create_collision_array(self, particle):
-        pass
